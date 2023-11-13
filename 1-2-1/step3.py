6979a480db47b04efa0cd24a422b6fadaf066a44
15693 <zachary.stevens@vtsd.com>
2023-11-13 08:40:58 -0500

    1-2-1

--- 1-2-1/step3.py
+++ 1-2-1/step3.py
@@ -1,24 +1,29 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
-
+import random as rand
 #-----game configuration----
 bob = trtl.Turtle()
 bob_color = "yellow"
 bob_shape = "circle"
 bob_fillcolor = 2
+score = 0
 #-----initialize turtle-----
 bob.shape(bob_shape)
 bob.fillcolor(bob_color)
 bob.shapesize(bob_fillcolor)
 
 #-----game functions--------
-def onclick(f):
-    print("bob was clicked")
-
+def bob_clicked(x, y):
+    change_position()
+def change_position():
+    bob.penup()
+    new_xpos = rand.randint(1, 400)
+    new_ypos = rand.randint(1, 400)
+    bob.goto(new_xpos, new_ypos)
+def update_score():
 #-----events----------------
-bob.onclick()
-
+bob.onclick(bob_clicked)
 
 
 
