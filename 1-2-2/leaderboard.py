76e875a1dc79b35cb87ddbaf90db8bd49d43de25
15693 <zachary.stevens@vtsd.com>
2023-11-28 08:40:55 -0500

    1-2-1

--- 1-2-2/leaderboard.py
+++ 1-2-2/leaderboard.py
@@ -45,11 +45,12 @@ def get_scores(file_name):
         index += 1
 
         # TODO 4: use a while loop to get the score
-        while line[index] != "/n":
+        while line[index] != "\n":
             leader_score += line[index]
             index += 1
         # TODO 5: add the player score to the scores list
         scores.append(leader_score)
+    leaderboard_file.close()
     # TODO 7: return the scores in place of the empty list
     return scores
 
@@ -58,30 +59,28 @@ def get_scores(file_name):
 def update_leaderboard(file_name, leader_names, leader_scores, player_name, player_score):
     index = 0
     # TODO 8: loop through all the scores in the existing leaderboard list
-    '''
-    for   :
+    for index in range(len(leader_scores)):
       # TODO 9: check if this is the position to insert new score at
-      if ():
+      if (player_score >= leader_scores[index]):
         break
       else:
         index = index + 1
-    '''
 
     # TODO 10: insert new player and score
-
+    leader_scores.insert(index, player_score)
+    leader_names.insert(index, player_name)
     # TODO 11: keep both lists at 5 elements only (top 5 players)
-
+    if (len(player_name) > 5):
+        player_name.pop(5)
+        player_score.pop(5)
     # TODO 12: store the latest leaderboard back in the file
-
-    '''
     leaderboard_file = open(file_name, "w")  # this mode opens the file and erases its contents for a fresh start
   
     # TODO 13 loop through all the leaderboard elements and write them to the the file
-    for   :
+    for index in range(len(leader_names)):
       leaderboard_file.write(leader_names[index] + "," + str(leader_scores[index]) + "\n")
   
     leaderboard_file.close()
-     '''
 
 
 # draw leaderboard and display a message to player
