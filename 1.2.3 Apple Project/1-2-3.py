c71379883428f865f66071ce45ea5601ebf3ff68
15693 <zachary.stevens@vtsd.com>
2023-12-18 07:25:24 -0500

    1-2-1

---
+++ 1.2.3 Apple Project/1-2-3.py
@@ -0,0 +1,119 @@
+#   a123_apple_1.py
+import turtle as trtl
+import random as rand
+
+# -----setup-----
+apple_image = "apple.gif"  # Store the file name of your shape
+pear_image = "pear.gif"
+
+wn = trtl.Screen()
+drawer = trtl.Turtle()
+global checkletter
+global letter
+letter = ()
+randint = int(rand.randint(0, 26))
+
+wn.setup(width=1.0, height=1.0)
+wn.addshape(apple_image)  # Make the screen aware of the new file
+wn.addshape(pear_image)  # Make the screen aware of the new file
+apple_letter_x_offset = 25
+apple_letter_y_offset = 50
+letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z']
+if randint < len(letter_list):
+  randint = randint
+else:
+  randint = int(rand.randint(0, 26))
+
+letter = letter_list[0, randint]
+
+apple = trtl.Turtle()
+apple.penup()
+wn.tracer(False)
+# -----functions-----
+def draw_apple(active_apple, letter):
+    active_apple.showturtle()
+    active_apple.shape(apple_image)
+    drawLetter(active_apple, letter)
+    wn.update()
+
+def drawLetter(active_apple, letter):
+    drawer.penup()
+    drawer.goto(active_apple.xcor() - apple_letter_x_offset, active_apple.ycor() - apple_letter_y_offset)
+    drawer.color("white")
+    drawer.clear()
+    drawer.write(letter, font=("Arial", 55, "bold"))
+
+def reset_apple(active_apple):
+    if letter_list:
+        letter = rand.choice(letter_list)
+        letter_list.remove(letter)
+        active_apple.goto(rand.randint(-270, 270), 160)
+        print(letter_list)
+        draw_apple(active_apple, letter)
+
+def drop_apple():
+    wn.tracer(True)
+    apple.goto(apple.xcor(), apple.ycor() - 400)
+    apple.hideturtle()
+    wn.tracer(False)
+    reset_apple(apple)
+
+
+# -----function calls-----
+draw_apple(apple, "a")
+wn.onkeypress(drop_apple, "a")
+draw_apple(apple, "b")
+wn.onkeypress(drop_apple, "b")
+draw_apple(apple, "c")
+wn.onkeypress(drop_apple, "c")
+draw_apple(apple, "d")
+wn.onkeypress(drop_apple, "d")
+draw_apple(apple, "e")
+wn.onkeypress(drop_apple, "e")
+draw_apple(apple, "f")
+wn.onkeypress(drop_apple, "f")
+draw_apple(apple, "g")
+wn.onkeypress(drop_apple, "g")
+draw_apple(apple, "h")
+wn.onkeypress(drop_apple, "h")
+draw_apple(apple, "i")
+wn.onkeypress(drop_apple, "i")
+draw_apple(apple, "j")
+wn.onkeypress(drop_apple, "j")
+draw_apple(apple, "k")
+wn.onkeypress(drop_apple, "k")
+draw_apple(apple, "l")
+wn.onkeypress(drop_apple, "l")
+draw_apple(apple, "m")
+wn.onkeypress(drop_apple, "m")
+draw_apple(apple, "n")
+wn.onkeypress(drop_apple, "n")
+draw_apple(apple, "o")
+wn.onkeypress(drop_apple, "o")
+draw_apple(apple, "p")
+wn.onkeypress(drop_apple, "p")
+draw_apple(apple, "b")
+wn.onkeypress(drop_apple, "q")
+draw_apple(apple, "q")
+wn.onkeypress(drop_apple, "r")
+draw_apple(apple, "r")
+wn.onkeypress(drop_apple, "s")
+draw_apple(apple, "s")
+wn.onkeypress(drop_apple, "t")
+draw_apple(apple, "t")
+wn.onkeypress(drop_apple, "u")
+draw_apple(apple, "u")
+wn.onkeypress(drop_apple, "v")
+draw_apple(apple, "v")
+wn.onkeypress(drop_apple, "w")
+draw_apple(apple, "w")
+wn.onkeypress(drop_apple, "x")
+draw_apple(apple, "x")
+wn.onkeypress(drop_apple, "y")
+draw_apple(apple, "y")
+wn.onkeypress(drop_apple, "z")
+draw_apple(apple, "z")
+wn.listen()
+wn.bgpic("background.gif")
+wn.mainloop()
