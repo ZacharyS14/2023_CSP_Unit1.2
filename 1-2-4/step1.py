20e0c40e80d389a53c053dddfe513ed7c6ea8bc5
15693 <zachary.stevens@vtsd.com>
2024-01-02 07:57:40 -0500

    1-2-4

--- 1-2-4/step1.py
+++ 1-2-4/step1.py
@@ -1,18 +1,16 @@
 import turtle as trtl
 import random as rand
 drawer = trtl
-path_width = 10
-wall_len = 5
+path_width = 5
+wall_len = 25
+wall_increment = 10
 def spiral():
     drawer.speed(0)
     x = 10
     y = 10
-    wall_length = 10
-    wall_increment = 15
-    door_width = 10
     drawer.penup()
     drawer.goto(0,0)
-    drawer.pendown() a
+    drawer.pendown()
     for step in range(26):
         if step <= 4:
             drawer.penup()
@@ -35,10 +33,10 @@ def draw_barriers():
     drawer.back(10 * 2)
     drawer.right(90)
 
-def random_door():
-    door = rand.randint(path_width*2,(wall_len - path_width*2))
-def random_barrier():
-    barrier = rand.randint(path_width*2,(wall_len-path_width*2))
+def random():
+    door = rand.randint(path_width * 2, (wall_len - path_width * 2))
+    barrier = rand.randint(path_width * 2, (wall_len - path_width * 2))
+
 
 spiral()
 wn = trtl.Screen()
