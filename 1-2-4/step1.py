aff860e38a78a49a9674ee2693c2909f5aab69c2
15693 <zachary.stevens@vtsd.com>
2024-01-03 10:05:51 -0500

    1-2-4

--- 1-2-4/step1.py
+++ 1-2-4/step1.py
@@ -1,43 +1,52 @@
 import turtle as trtl
 import random as rand
-drawer = trtl
-path_width = 5
-wall_len = 25
-wall_increment = 10
-def spiral():
-    drawer.speed(0)
-    x = 10
-    y = 10
-    drawer.penup()
-    drawer.goto(0,0)
-    drawer.pendown()
-    for step in range(26):
-        if step <= 4:
-            drawer.penup()
-        else:
+# maze configuration variables
+num_sides = 25
+path_width = 15
+wall_color = "black"
+
+# config maze
+drawer = trtl.Turtle()
+drawer.pensize(5)
+drawer.pencolor(wall_color)
+drawer.speed("fastest")
+
+def drawSpiral():
+    wall_len = path_width
+    for w in range(num_sides):
+        wall_len += path_width
+
+        if (w > 4):
+            # Initial turn for painter to be in the correct direction
             drawer.left(90)
-            draw_door()
-            draw_barriers()
-            drawer.forward(x + y - 30 - 10)
-            y += 10
+            #create random #
+            randomnum = rand.randint(1,2)
+            if randomnum == 1:
+                print("door first")
+                draw_door()
+                draw_barrier()
+            else:
+                print("barrier first")
+                draw_barrier()
+                draw_door()
+            drawer.forward(wall_len - 10 - path_width - 40)
 
 def draw_door():
     drawer.forward(10)
     drawer.penup()
-    drawer.forward(10 * 2)
+    drawer.forward(path_width * 2)
     drawer.pendown()
-def draw_barriers():
+
+def draw_barrier():
+    # draw barrier wall
     drawer.forward(40)
     drawer.left(90)
-    drawer.forward(10 * 2)
-    drawer.back(10 * 2)
+    drawer.forward(path_width * 2)
+    drawer.backward(path_width * 2)
     drawer.right(90)
 
-def random():
-    door = rand.randint(path_width * 2, (wall_len - path_width * 2))
-    barrier = rand.randint(path_width * 2, (wall_len - path_width * 2))
+drawSpiral()
+drawer.hideturtle()
 
-
-spiral()
 wn = trtl.Screen()
 wn.mainloop()
